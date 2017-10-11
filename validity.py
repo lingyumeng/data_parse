3a20a0cfde2aef8931ba31d3246ff26619e92c7b
lw <jianyoulian@hotmail.com>
2017-10-11 17:32:53 +0800

    1011

--- validity.py
+++ validity.py
@@ -18,3 +18,32 @@ The following things should be done:
 You can write helper functions for checking the data and writing the files, but we will call only the
 'process_file' with 3 arguments (inputfile, output_good, output_bad).
 """
+
+import csv
+import pprint
+
+INPUT_FILE = 'autos.csv'
+OUTPUT_GOOD = 'autos-valid.ccv'
+OUTPUT_BAD = 'FIXME-autos.csv'
+
+def process_file(input_file, output_good, output_bad):
+    with open(input_file, 'r') as f:
+        reader = csv.DictReader(f)
+        header = reader.fieldnames
+
+        # COMPLETE THIS FUNCTION
+
+
+    # This is just an example on how you can use csv.DictWriter
+    # Remember that you have to output 2 files
+    with open(output_good, 'w') as g:
+        writer = csv.DictWriter(g, delimiter=',', fieldnames=header)
+        writer.writeheader()
+        for row in YOURDATA:
+            writer.writerow(row)
+
+def test():
+    process_file(INPUT_FILE, OUTPUT_GOOD, OUTPUT_BAD)
+
+if __name__ == '__main__':
+    test()
