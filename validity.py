ad47b27e5a857740b90e89a73e63bd231517b6be
lw <jianyoulian@hotmail.com>
2017-10-12 15:09:42 +0800

    1011

--- validity.py
+++ validity.py
@@ -20,27 +20,69 @@ You can write helper functions for checking the data and writing the files, but 
 """
 
 import csv
-import pprint
+import time
+#import pprint
 
 INPUT_FILE = 'autos.csv'
-OUTPUT_GOOD = 'autos-valid.ccv'
+OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
 
 def process_file(input_file, output_good, output_bad):
+    g_list = []
+    b_list = []
+
+    def is_date(datestr):
+        try:
+            time.strptime(datestr, '%Y-%m-%d')
+            return True
+        except:
+            return False
+
+
+
     with open(input_file, 'r') as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
 
-        # COMPLETE THIS FUNCTION
+        while True:
+            try:
+                line = reader.next()
+                if line['URI'].find("dbpedia.org") < 0:
+                    continue
 
+                datestr = line['productionStartYear'][:10]
+                print datestr
+
+                if is_date(datestr):
+                    if int(datestr[:4]) > 1886 and int(datestr[:4]) < 2004:
+                        line['productionStartYear'] = time.strptime(datestr[:4], '%Y').tm_year
+                        print(line['productionStartYear'])
+                        g_list.append(line)
+                    else:
+                        b_list.append(line)
+                else:
+                    b_list.append(line)
+
+            except:
+                break
+
+        # COMPLETE THIS FUNCTION
+    print len(g_list)
+    print len(b_list)
 
     # This is just an example on how you can use csv.DictWriter
     # Remember that you have to output 2 files
     with open(output_good, 'w') as g:
         writer = csv.DictWriter(g, delimiter=',', fieldnames=header)
         writer.writeheader()
-        for row in YOURDATA:
+        for row in g_list:
             writer.writerow(row)
+
+    with open(output_bad, 'w') as b:
+        bwriter = csv.DictWriter(b, delimiter=',', fieldnames=header)
+        bwriter.writeheader()
+        for brow in b_list:
+            bwriter.writerow(brow)
 
 def test():
     process_file(INPUT_FILE, OUTPUT_GOOD, OUTPUT_BAD)
